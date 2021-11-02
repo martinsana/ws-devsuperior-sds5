b9f8213dae6aedf55bb8a9390f0e1a6f6e067461
Ana Paula <anapaula.martins17@hotmail.com>
2021-11-01 22:28:51 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
+import DataTable from 'components/DataTable';
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+        <DataTable />
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
